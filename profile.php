126f8838f61d6c2a9f01350d24e22751b281f20d
Richard Beattie <richard.beattie@aol.co.uk>
2018-12-16 18:55:43 +0000

    Expanded profile
    The user's profile now shows a list of all their uploaded calls and statistics about those calls

--- profile.php
+++ profile.php
@@ -2,6 +2,9 @@
   session_set_cookie_params(0, '/', '.batidentification.com');
   session_start();
   require("libraries/dbconnect.php");
+  if(!isset($_SESSION['username'])){
+    header("Location: index.php");
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -42,37 +45,88 @@
   <body>
     <?php
       include("includes/navigation.php");
+      $calls = array();
+      $verifiedCalls = 0;
+      $analyzedCalls = 0;
+      $stmt = $connection->prepare("SELECT DATE(date_recorded), call_url, classification, address, verified, analyzed FROM bat_calls WHERE user_id = ?");
+      $stmt->bind_param("i", $_SESSION['id']);
+      $stmt->execute();
+      $stmt->bind_result($date_recorded, $call_url, $specie, $address, $verified, $analyzed);
+      while($stmt->fetch()){
+          $verifiedCalls += $verified;
+          $analyzedCalls += $analyzed;
+          array_push($calls, [$date_recorded, $call_url, ucwords(str_replace("_", " ", $specie)), $address, $verified, $analyzed]);
+      }
+      $stmt->close();
     ?>
     <h5 id="warning-label"></h3>
     <div class="content container-fluid central-box">
       <div class="row">
-        <div class="col-md-12 profile-header">
-          <h3>Welcome <?php echo($_SESSION['username']) ?>!</h3>
-          <ul class="nav navbar-nav">
-            <li><a>My calls</a></li>
-            <li><a>Settings</a></li>
-            <li><a id="upload">Upload a new call</a></li>
-            <li><a href="actions/logout.php">Logout</a></li>
-          </ul>
-          <hr>
+        <div class="col-md-9 container-fluid user-content">
+          <div class="row profile-header">
+            <div class="col-md-12">
+              <h3>Welcome <?php echo($_SESSION['username']) ?>!</h3>
+              <ul class="nav navbar-nav">
+                <li><a id="upload">Upload a new call</a></li>
+                <li><a>Settings</a></li>
+                <li><a href="actions/logout.php">Logout</a></li>
+              </ul>
+              <hr>
+            </div>
+          </div>
+          <div class="row uploaded-calls">
+            <div class="col-md-12">
+                <?php
+
+                  foreach($calls as $call){
+                      $specie = $call[2] == '' ? 'Unknown' : $call[2];
+                      $verified = ($call[4] == 1 ? "" : "text-muted");
+                      $analyzed = ($call[5] == 1 ? "" : "text-muted");
+                      echo("
+                          <div class='uploaded-call container-fluid'>
+                            <div class='row'>
+                              <div class='col-md-10'>
+                                <h4>{$specie}</h4>
+                                <p>{$call[0]} {$call[3]}</p>
+                                <b class='{$verified}'>Verified</b>
+                                <b class='{$analyzed}'>Analyzed</b>
+                              </div>
+                              <div class='col-md-2 call-actions'>");
+                      if($call[4]){
+                        echo("<a href='{$call[1]}spectrogram.png'><i class='fas fa-image fa-3x'></i></a>");
+                      }
+                      if($call[5]){
+                        echo("<a href='{$call[1]}time_expansion.wav'><i class='fas fa-file-audio fa-3x'></i></a>");
+                      }
+                      echo('
+                              </div>
+                            </div>
+                          </div>
+                      ');
+                  }
+
+                ?>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 container-fluid user-stats">
+          <div class="row profile-header">
+              <div class="col-md-12">
+                <h3>Statistics</h3>
+              </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+                <span class="stat"><b><?php echo(count($calls));?></b> calls uploaded</span>
+                <br>
+                <span class="stat-sub"><b><?php echo($analyzedCalls);?></b> have been analyzed</span>
+                <br>
+                <span class="stat-sub"><b><?php echo($verifiedCalls);?></b> have been verified</span>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="row">
-        <div class="col-md-12 profile-stats">
-            <?php
-
-              $stmt = $connection->prepare("SELECT COUNT(*) FROM bat_calls WHERE user_id = ?");
-              $stmt->bind_param("i", $_SESSION['id']);
-              $stmt->execute();
-              $stmt->bind_result($cnt);
-              $stmt->fetch();
-              $stmt->close();
-
-              echo("<span><h4>{$cnt}</h4> calls uploaded</span>")
-
-            ?>
-        </div>
-      </div>
+    </div>
     </div>
     <div class="overlay">
         <div class="upload-container" >
