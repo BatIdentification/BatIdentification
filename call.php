daadebea2852e501cfd7e7390cf55d2551316086
Richard Beattie <richard.beattie@aol.co.uk>
2019-03-31 17:24:32 +0100

    Switched resource urls to for .htaccess

--- call.php
+++ call.php
@@ -42,12 +42,12 @@
   <head>
     <title>Call</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="libraries/Spectrogram-Player/spectrogramplayer.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/libraries/Spectrogram-Player/spectrogramplayer.css">
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="libraries/Spectrogram-Player/spectrogramplayer.js"></script>
+    <script type="text/javascript" src="/libraries/Spectrogram-Player/spectrogramplayer.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script>
       $(document).ready(function(){
