b49b8cac76545d87ccbe3488606be905c6392753
Richard Beattie <richard.beattie@aol.co.uk>
2019-04-10 20:55:40 +0100

    Fixes logout

--- actions/logout.php
+++ actions/logout.php
@@ -1,5 +1,7 @@
 <?php
 
+  session_name("batidentification");
+  session_set_cookie_params(0, '/', '.batidentification.com');
   session_start();
   session_destroy();
   header("Location: ../index.php");
